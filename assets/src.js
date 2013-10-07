c5a47fef7b9edbf97ad7459f1877cc6b36512527
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2013-10-07 18:37:56 +0000

    Make credits work on WebKit browsers.

--- assets/src.js
+++ assets/src.js
@@ -2,9 +2,10 @@ window.onload =
     function() {
         var footer1 = document.getElementById("credits");
         var footer2 = document.getElementById("creditsfull");
-        
-        footer1.addEventListener("mouseenter", function() { 
+        var cb = function() { 
             footer1.style.display = "none";
             footer2.style.display = "inline";
-        })
+	    footer1.removeEventListener("mouseover", cb);
+        };
+        footer1.addEventListener("mouseover", cb);
     };
