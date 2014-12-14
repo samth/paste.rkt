1628f0f63771570685e419130618ce412b6a9f3d
Sam Tobin-Hochstadt <samth@ccs.neu.edu>
2014-12-14 08:15:07 -0500

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -1,3 +1,6 @@
 #lang info
 (define collection "paste_rkt")
-(define deps '("git://github.com/samth/mongodb"))
+(define deps '("base"
+               "sandbox-lib"
+               "web-server-lib"
+               "git://github.com/samth/mongodb"))
